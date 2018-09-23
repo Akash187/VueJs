f4ce61f5d750ebe0d3ae62fa6d5efd349191e942
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-09-23 17:20:52 +0530

    Data & Methods

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-console.log("Hello");
-
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Akash Kumar'
+    name: 'Akash Kumar',
+    job: 'Web Developer'
+  },
+  methods: {
+    greet: function (time) {
+      return `Good ${time} ${this.name}`
+    }
   }
 });
